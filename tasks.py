deffb52f560180a1cbf0fc63868844e9d2731b98
KasraMousapour <kasramousapour84@gmail.com>
2025-10-11 08:10:49 +0330

    feat: add some methods to task class

--- tasks.py
+++ tasks.py
@@ -1,24 +1,83 @@
 from typing import Literal
+from datetime import datetime
 class Task:
     names_list = []
     count = 0
-    def __init__(self,name:str,description:str,status:Literal["done" ,"doing" ,"todo"]="todo"):
+    _name : str
+    _description : str
+    _status:Literal["done" ,"doing" ,"todo"]="todo"
+    _deadline:str
+
+    def __init__(self,name:str,description:str,status:Literal["done" ,"doing" ,"todo"]="todo",deadline:str=""):
         if Task.count > MAX_NUMBER_OF_TASK:
             raise ValueError("The number of tasks is full")
+        else:
+            self.name(name)
+            self.description(description)
+            self.status(status)
+            self.deadline(deadline)
+            Task.names_list.append(name)
+            count += 1
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @property
+    def status(self):
+        return self._status
+    
+    @property
+    def deadline(self):
+        return self._deadline
+
+    @name.setter
+    def name(self, name:str):
+        if not name:
+            raise ValueError("Name cannot be empty.")
         elif name in Task.names_list:
             raise ValueError("The name is used")
         elif len(name.split()) > 30:
             raise ValueError("The name must be at least 30 words")
-        elif len(description.split()) > 150:
+        else:
+            self._name = name    
+
+    @description.setter
+    def description(self, description:str):
+        if len(description.split()) > 150:
             raise ValueError("The description must be at least 150 words")
         else:
-            self.name = name
-            self.description = description
-            self.status = status
-            Task.names_list.append(name)
-            count += 1
+            self._description = description
 
-    def change_status(self, new_status:Literal["done" , "doing"]):
+    @status.setter
+    def status(self, status:Literal["done" ,"doing" ,"todo"]="todo"):
+        try:
+            self._status = status
+        except ValueError:
+            print("The status must be in done or doing mode.")    
+
+    @deadline.setter
+    def deadline(self, deadline):
+        if deadline:
+            try:
+                datetime.strptime(deadline, "%Y-%m-%d")
+                self._deadline = deadline
+            except ValueError:
+                print("the deadline format must be YYYY-MM-DD")
+
+
+    def update_status(self, new_status:Literal["done" , "doing"]):
         self.status = new_status
         print("successfully status changed.")
-                
+
+    def update_name(self, new_name):
+        self.name(new_name)
+    
+        
+
+        
+
