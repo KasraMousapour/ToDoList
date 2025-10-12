dd27542caf88253411a85b2a91c788b372075dd9
KasraMousapour <kasramousapour84@gmail.com>
2025-10-12 20:02:45 +0330

    feat: add setter to Project class atters

--- projects.py
+++ projects.py
@@ -4,17 +4,36 @@ class Project:
     def __init__(self,name:str,description:str):
         if Project.count > MAX_NUMBER_OF_PROJECT:
             raise ValueError("The number of projects is full")
-        elif name in Project.names_list:
+        else:
+            self.name(name)
+            self.description(description)
+            Project.names_list.append(name)
+            count += 1
+
+    @property
+    def name(self):
+        return self._name   
+    
+    @property
+    def description(self):
+        return self._description
+    
+    @name.setter
+    def name(self, name:str):
+        if name in Project.names_list:
             raise ValueError("The name is used")
         elif len(name.split()) > 50:
             raise ValueError("The name must be at least 50 words")
-        elif len(description.split()) > 150:
+        else:
+            self._name = name
+
+    @description.setter
+    def description(self, description:str):
+        if len(description.split()) > 150:
             raise ValueError("The description must be at least 150 words")
         else:
-            self.name = name
-            self.description = description
-            Project.names_list.append(name)
-            count += 1
+            self._description = description
+
     def update_name(self,new_name:str):
         if new_name in Project.names_list:
             raise ValueError("The new name is used")
