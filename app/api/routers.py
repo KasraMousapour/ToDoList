715ac4e961c63fb7cb16c2a92d625fdc520de2f4
KasraMousapour <kasramousapour84@gmail.com>
2025-11-22 10:17:46 +0330

    style: optimization urls

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,5 +3,5 @@ from api.controllers import projects_controller, tasks_controller
 
 router = APIRouter()
 
-router.include_router(projects_controller.router, prefix="/projects", tags=["Projects"])
-router.include_router(tasks_controller.router, prefix="/tasks", tags=["Tasks"])
+router.include_router(projects_controller.router, prefix="/api/projects", tags=["Projects"])
+router.include_router(tasks_controller.router, prefix="/api/tasks", tags=["Tasks"])
