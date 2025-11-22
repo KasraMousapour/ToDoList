04753b4552a670688ca6924e5f0280178f90d545
KasraMousapour <kasramousapour84@gmail.com>
2025-11-22 09:31:11 +0330

    feat: add init files

---
+++ app/api/controllers/__init__.py
@@ -0,0 +1,3 @@
+# Expose routers from controllers
+from .projects_controller import router as projects_router
+from .tasks_controller import router as tasks_router
---
+++ app/api/controllers_schema/__init__.py
@@ -0,0 +1,7 @@
+# Expose request schemas
+from .requests.projects_request_schema import ProjectCreateRequest, ProjectUpdateRequest
+from .requests.tasks_request_schema import TaskCreateRequest, TaskUpdateRequest
+# Expose response schemas
+from .responses.projects_responses_schema import ProjectResponse
+from .responses.tasks_responses_schema import TaskResponse
+
